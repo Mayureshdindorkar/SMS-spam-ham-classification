83c3a02e3b0005ab5b6b04b1bc05134084dc265f
Mayuresh Dindorkar <dmayuresh99@gmail.com>
2020-12-01 20:51:01 +0530

    new files uploaded

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request
 import pickle
 
-filename = 'mypipeline.pkl'
-pipe_clf = pickle.load(open(filename, 'rb'))
+rf_clf = pickle.load(open('randomforest_model.pkl', 'rb'))
+tfidf = pickle.load(open('tfidffile.pkl', 'rb'))
 
 app = Flask(__name__)
 
@@ -14,8 +14,21 @@ def home():
 def predict():
     if request.method == 'POST':
     	message = request.form['message']
-    	my_prediction = pipe_clf.predict([message])
-    	return render_template('result.html', prediction=my_prediction)
+		# validations for messages
+    	#my_prediction = pipe_clf.predict([message])
+		if len(message) == 0:
+			return render_template('<h2>!!!! Please enter valid SMS !!!!<h2>')
+		else:
+			sample_message = re.sub(pattern='[^a-zA-Z]',repl=' ', string = message)
+			sample_message = sample_message.lower()
+			sample_message_words = sample_message.split()
+			sample_message_words = [word for word in sample_message_words if not word in set(stopwords.words('english'))]
+			final_message = [wnl.lemmatize(word) for word in sample_message_words]
+			final_message = ' '.join(final_message)
+
+			temp = tfidf.transform([final_message]).toarray()
+			my_prediction = rf.predict(temp)
+    		return render_template('result.html', prediction=my_prediction)
 
 if __name__ == '__main__':
 	app.run(debug=True)
